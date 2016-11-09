9fe28fe92977bcff4aa0ec5e17f7f394be32b332
gataro3dev <cabiche22@gmail.com>
2016-11-08 22:25:59 -0500

    pagina 4

---
+++ Assets/Scripts/pagina 4/moverObjeto.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moverObjeto : MonoBehaviour {
+	bool control;
+	public string nombre;
+	public GameObject texto;
+	// Use this for initialization
+	void Start () {
+		control = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!control){
+			mover ();
+		}
+	}
+
+	void mover(){
+		transform.Translate (80 * Time.deltaTime, 0, 0);
+	}
+
+	void OnTriggerEnter(Collider obj){
+		GameObject otro = GameObject.Find (nombre);
+		if (obj.gameObject.name == "centro") {
+			GameObject nuevo = Instantiate (texto);
+			nuevo.transform.SetParent (GameObject.Find("textoref").transform);
+		}
+		if(obj.gameObject.name == "objectRef"){
+			control = true;
+			Destroy (this.gameObject);
+			Destroy (texto);
+		}
+	}
+}
